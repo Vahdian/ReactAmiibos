d6c039a36e22851de052264fd40c9172dafbc5c3
vahdian <germanpalero@gmail.com>
2021-02-20 18:00:59 +0100

    form styles

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import './ContactPage.scss'
  
 export function ContactPage(){
     const{ register, handleSubmit, watch, errors, reset} = useForm()
@@ -9,15 +10,19 @@ export function ContactPage(){
  }
     return(
         <div>
-            <form onSubmit={handleSubmit(doSubmit)}>
-                <label>Name
-                    <input type="text" name="name" ref={register({required: true, minLength:4})}></input>
+            <h1>Get info about the amiibos!</h1>
+            <form onSubmit={handleSubmit(doSubmit)} className="formPage">
+                <label>
+                    <input type="text" name="name" ref={register({required: true, minLength:4})} className="formInput" placeholder="Insert your name"></input>
                     {errors.name && <span>Name needs to be longer than 4 chars</span>}
                 </label>
-                <label>Surname
-                    <input type="text" name="suname" ref={register}></input>
+                <label>
+                    <input type="text" name="surname" ref={register} className="formInput" placeholder="Insert your surname"></input>
                 </label>
-                <input type="submit" value="SEND"></input>
+                <label>
+                    <input type="text" name="email" ref={register} className="formInput" placeholder="E-mail"></input>
+                </label>
+                <input type="submit" value="SEND" className="formButton"></input>
             </form>
         </div>
     )
